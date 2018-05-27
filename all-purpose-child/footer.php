0d131dcf8afff2da8dd03c182acc925b93a23baa
krisjansson <krisjansson@users.noreply.github.com>
2018-05-27 21:46:09 +0200

    Website Code
    Alle customer codning som er brugt på hjemmesiden

---
+++ all-purpose-child/footer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * @package All Purpose
+ */
+
+?>
+
+	</div><!-- #content -->
+	
+	<footer id="colophon"  role="contentinfo">
+	
+		<div class="site-info">
+		    <div class="table">
+			
+			<!-- CUSTOM HTML START -->
+			
+			<div class="main">
+                <div class="a"><a href="/illustrator/">Hjem</a> <a href="/illustrator/tutorials/">Tutorials</a> <a href="/illustrator/genveje-illustrationer/">Genveje</a></div>
+                <div class="b">Hjemmeside lavet af<a target="_blank" href="http://kristjansson.dk/"> kristjansson.dk </a> &amp;<a href="http://jekes.dk/" target="_blank"> jekes.dk </a></div>
+                    <div class="c">Alle grafiske elementer er designet med Adobe Illustrator CC</div>
+</div>
+			<!-- CUSTOM HTML END -->
+			
+		</div><!-- .site-info -->
+		
+		</div>
+		
+	</footer><!-- #colophon -->
+	
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
